6e60e01c64d8decc7edd05d1b74a27fa60574393
Sanjay D <josephstudent001@gmail.com>
2026-07-14 13:18:14 +0530

    Update code.java

--- ZOHO/DSA Problems/Easy/Print Mismatched Characters Between Two Strings/code.java
+++ ZOHO/DSA Problems/Easy/Print Mismatched Characters Between Two Strings/code.java
@@ -1,41 +1,29 @@
 import java.util.Scanner;
 
+// "static void main" must be defined in a public class.
 public class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
-        String s1 = sc.next();
-        String s2 = sc.next();
-
-        boolean found = false;
-        int i = 0;
-
-        while (i < s1.length()) {
-
-            if (s1.charAt(i) != s2.charAt(i)) {
-
-                StringBuilder a = new StringBuilder();
-                StringBuilder b = new StringBuilder();
-
-                while (i < s1.length() && s1.charAt(i) != s2.charAt(i)) {
-                    a.append(s1.charAt(i));
-                    b.append(s2.charAt(i));
-                    i++;
-                }
-
-                System.out.println(a + " , " + b);
-                found = true;
-
-            } else {
+        
+        String st1 = sc.next();
+        String st2 = sc.next();
+        
+        int n = st1.length();
+        
+        for(int i=0; i<n-1; i++){
+            char ch1 = st1.charAt(i);
+            char ch2 = st2.charAt(i);
+            
+            char ch3 = st1.charAt(i+1);
+            char ch4 = st2.charAt(i+1);
+            
+            if(ch1 != ch2 && ch3 != ch4){
+                System.out.println(ch1 + "" + ch3 + ", " + ch2 + "" + ch4 + " ");
                 i++;
             }
+            else if(ch1 != ch2){
+                System.out.println(ch1 + ", " + ch2);
+            }
         }
-
-        if (!found) {
-            System.out.println("No mismatches");
-        }
-
-        sc.close();
     }
 }
